559529ccde78c63dc36a0f95ad18e888db0efe0e
John Daugherty <johndaugherty1986@gmail.com>
2015-07-16 15:51:17 -0500

    ADDED NEW STUFF TO IMAGE MODEL FOR CHEESE FUCKKKKKKK

--- app/models/cheese.rb
+++ app/models/cheese.rb
@@ -2,7 +2,11 @@ class Cheese < ActiveRecord::Base
   has_many :reviews
   has_many :wheels
   belongs_to :farm
-  has_attached_file :image, styles: { large: "600x600>", medium: "300x300", thumb: "100x100#"}
+  has_attached_file :image, styles: { large: "600x600>",
+                                      medium: "300x300",
+                                      thumb: "100x100#"},
+                    :url => ":s3_domain_url",
+                    :path => "/:class/:attachment/:id_partition/:style/:filename"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   def average_rating
